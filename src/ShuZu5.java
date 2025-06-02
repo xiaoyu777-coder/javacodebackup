aefb05cc018e00a27fd4b4f7640a6c25d3a189a2
nbdexiaoyu <asd201390123456@gmail.com>
2025-06-02 12:30:05 +0800

    1

--- src/ShuZu5.java
+++ src/ShuZu5.java
@@ -1,6 +1,6 @@
 public class ShuZu5 {
     public static void main(String[] args) {
-        // 创建一个长度为3的整数数组
+        // 创建一个长度为3的整数数组，动态初始化数组元素为0
         int[] arr = new int[3];
         // 为数组的第一个元素赋值为1
         arr[0] = 1;
