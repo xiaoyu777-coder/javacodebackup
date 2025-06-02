78951fbdf9fd85954e1163092a5aedda1ac93ac0
nbdexiaoyu <asd201390123456@gmail.com>
2025-06-02 12:45:15 +0800

    简化

--- src/ShuZu6.java
+++ src/ShuZu6.java
@@ -2,7 +2,7 @@ public class ShuZu6 {
     public static void main(String[] args) {
         int[] arr = { 33, 5, 22, 44, 55 };
         int max = arr[0];
-        for (int i = 0; i < arr.length; i++) {
+        for (int i : arr) {
             if (arr[i] > max){
                 max = arr[i];
             }
