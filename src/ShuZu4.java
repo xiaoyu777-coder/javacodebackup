e64a1588a51f652a01aacf97914065355c65d8ba
nbdexiaoyu <asd201390123456@gmail.com>
2025-06-02 12:08:45 +0800

    update name

--- src/ShuZu4.java
+++ src/ShuZu4.java
@@ -1,4 +1,4 @@
-public class ShuZhu4 {
+public class ShuZu4 {
     public static void main(String[] args) {
         // 初始化一个整数数组
         int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
---
+++ src/ShuZu5.java
@@ -0,0 +1,5 @@
+public class ShuZu5 {
+    public static void main(String[] args) {
+        int[] arr = new int[3];
+    }
+}
