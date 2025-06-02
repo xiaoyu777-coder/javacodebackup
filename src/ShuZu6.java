4afe3ae631960acc9152d7930e57bbfb4ada7889
nbdexiaoyu <asd201390123456@gmail.com>
2025-06-02 12:40:57 +0800

    新1

--- src/ShuZu6.java
+++ src/ShuZu6.java
@@ -1,5 +1,9 @@
 public class ShuZu6 {
     public static void main(String[] args) {
-
+        int[] arr = { 33, 5, 22, 44, 55 };
+        int max = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i]
+        }
     }
 }
