2bb39864de29ce08823fbcace83187a287c00b89
nbdexiaoyu <asd201390123456@gmail.com>
2025-06-02 12:24:10 +0800

    注释更改

--- src/ShuZu5.java
+++ src/ShuZu5.java
@@ -1,10 +1,15 @@
 public class ShuZu5 {
     public static void main(String[] args) {
+        // 创建一个长度为3的整数数组
         int[] arr = new int[3];
+        // 为数组的第一个元素赋值为1
         arr[0] = 1;
+        // 为数组的第二个元素赋值为2
         arr[1] = 2;
+        // 遍历数组并打印每个元素的值
         for (int i : arr) {
            System.out.println(arr[i]);
         }
     }
 }
+
