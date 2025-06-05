2621e39c143ca2d46109daa53a5a36b1d0750e7c
nbdexiaoyu <asd201390123456@gmail.com>
2025-06-05 13:46:19 +0800

    练习方法

---
+++ src/FangFa.java
@@ -0,0 +1,45 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class FangFa {
+    static void caishuzi() {
+        Random rand = new Random();
+        int randomNum = rand.nextInt(100) + 1;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("请输入你要猜的数字：");
+        while (true) {
+            int randomNum2 = sc.nextInt();
+            if (randomNum2 < randomNum) {
+                System.out.println("猜小了");
+            } else if (randomNum2 > randomNum) {
+                System.out.println("猜大了");
+            } else {
+                System.out.println("猜对了!!!");
+                break;
+            }
+        }
+    }
+    static void FengQiGuo() {
+        System.out.println("输入小于100的数字：");
+        Scanner sc = new Scanner(System.in);
+        int i = sc.nextInt();
+        for (; i <= 100; i++) {
+            if (i % 10 == 7 || i / 10 % 10 == 7 || i % 7 == 0) {
+                System.out.println("过");
+                continue;
+            }
+            System.out.println(i);
+
+        }
+    }
+    public static void main(String[] args) {
+        System.out.println("选择游戏：1.猜数字 2.逢7过");
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        if (num == 1) {
+            caishuzi();
+        }else{
+            FengQiGuo();
+        }
+    }
+}
